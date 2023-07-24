0532358ac104f0adee69d7f349233f3407996c9b
Shola Onoriemu <pyshola@gmail.com>
2023-07-25 00:38:55 +0100

    arrow_function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,8 @@ export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
     const self = this;
-    this.addNeighborhood = (newNeighborhood) =>{
+    let add;
+    this.addNeighborhood = add = (newNeighborhood) =>{
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
     };
