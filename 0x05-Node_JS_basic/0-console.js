0ee622fed98086cf4f16e2d362e48f5fe0609cc4
Shola Onoriemu <pyshola@gmail.com>
2023-09-19 21:12:53 +0100

    node api

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Shola Onoriemu <https://github.com/pyshola>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
